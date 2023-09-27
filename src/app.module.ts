2709133b16059efacc9f708c858e72572fea120e
Maher Abaza <maher.abaza@protonmail.com>
2023-09-28 02:39:04 +0300

    fix: configure env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { LogModule } from './log/log.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from 'ormconfig';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     LogModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       ...ormConfig,
       autoLoadEntities: true,
